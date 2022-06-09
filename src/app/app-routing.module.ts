290642157d402bee22230cfe00a42c41764e1a2f
Emanuel Contreras <emanuelcf@outlook.es>
2022-06-08 18:43:54 -0600

    Formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"Formulario",component:FormularioComponent},
+  {path:"**", component:FormularioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
